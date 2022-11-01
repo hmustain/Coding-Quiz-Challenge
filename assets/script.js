64fd722275d3902a7e8e770de7a5286813d61e4a
hmustain <hunter@bunkerbranding.com>
2022-11-01 11:22:53 -0500

    adding a variable questions and assigning an array of questions to it

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+// Let's start with creating a variable with an array to store our questions
+
+var questions = [
+    {
+        question: "What is a useful tool that developers often use to output a message to the web console?",
+        choices: ["Conditional Statements", "Functions", "Console.log", "GitHub"],
+        answer: "Console.log"
+    },
+{
+        question: "What method do you use to access an element by ID?",
+        choices: ["Document.getElementById", "Window.getElementById", "Alert.getElementById", "Prompt.getElementById"],
+        answer: "Document.getElementById"
+},
+{
+        question: "Commonly used data types DO NOT include:",
+        choices: ["Integers", "Alerts", "Strings", "Booleans"],
+        answer: "Alerts"
+},
+{
+        question: "The condition in an if/else statement is enclosed with __________",
+        choices: ["Quotes", "Curly brackets", "Square brackets", "Parentheses"],
+        answer: "Parentheses"
+},
+{
+        question: "Arrays in JavaScript can be used to store __________",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+        answer: "All of the above"
+},
+{
+        question: "String values must be enclosed within __________ when being assigned to variables",
+        choices: ["Quotes", "Commas", "Curly brackets", "Parentheses"],
+        answer: "Quotes"
+},
+{
+        question: "The __________ method is used to convert a JavaScript value to a JSON string",
+        choices: ["JSON parse", "JSON.stringify", "Join", "Prevent default"],
+        answer: "JSON.stringify"
+},
+{
+        question: "The __________ method is used to convert a JSON string back to a JavaScript value",
+        choices: ["JSON.stringify", "Prevent default", "JSON.parse", "Switch"],
+        answer: "JSON.parse"
+},  
+],
